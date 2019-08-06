6cda9303cccb5e392fdcdda4d738d8fe26b442ee
timothydowd <timdowd1984@gmail.com>
2019-08-06 15:53:14 +0100

    checking git ignore

--- main.js
+++ main.js
@@ -1,91 +1,11 @@
-// import data from './data.json'
+const {formatJsonData, fetchWordInfos, extractRootWords} = require('./utils') 
 const data = require('./data.json')
-const axios = require('axios');
-
-const formatJsonData = (json) => {
-    const formattedData = json.reduce((reducedData, object) => {
-        const word = object["Word"]
-        const synonyms = [object["Synonym 1"], object["Synonym 2"], object["Synonym 3"], object["Synonym 4"]]
-        const antonyms = [object["Antonym 1"], object["Antonym 2"], object["Antonym 3"], object["Antonym 4"]]
-
-        const removedBlankSynonyms = synonyms.reduce((reducedSynonyms, synonym) => {
-            return synonym === "" ? [...reducedSynonyms] : [ ...reducedSynonyms, synonym]    
-        },[])
-
-        const removedBlankAntonyms = antonyms.reduce((reducedAntonyms, antonym) => {
-            return antonym === "" ? [...reducedAntonyms] : [ ...reducedAntonyms, antonym]    
-        },[])
-
-        return [ ...reducedData, { word, synonyms: removedBlankSynonyms, antonyms: removedBlankAntonyms}]
-    },[])
-
-   
-    return formattedData
-}
-
-const formattedData = formatJsonData(data)
+// const  = require('./utils') 
+// const fetchWordInfos from './utils'
 
 
-const addMissingData = (formattedData) => {
+// fetchWordInfos(extractRootWords(data))
+const words = extractRootWords(data)
 
-}
-
-//// call api
-
-const words = ['ace', 'place', 'set']
-
-
-const savedData = words.map(word => {
-
-//     // const http = require("https");
-
-//     // const app_id = "6a753563"; // insert your APP Id
-//     // const app_key = "4ed66b63acb3c546838c88c7ab5d3c12"; // insert your APP Key
-//     // const wordId = word;
-//     // const fields = "";
-//     // const strictMatch = "false";
-
-//     // const options = {
-//     // host: 'od-api.oxforddictionaries.com',
-//     // port: '443',
-//     // path: '/api/v2/entries/en-gb/' + wordId + '?fields=' + fields + '&strictMatch=' + strictMatch,
-//     // method: "GET",
-//     // headers: {
-//     //     'app_id': app_id,
-//     //     'app_key': app_key
-//     // }
-//     // };
-
-const URL = `https://od-api.oxforddictionaries.com/api/v2/entries/en-us/${word}?strictMatch=false`
-
-return axios.get(URL, { headers: { app_id: "6a753563", app_key: "4ed66b63acb3c546838c88c7ab5d3c12" }})
-  .then(function (response) {
-    // handle success
-    return response.data
-  })
-  .catch(function (error) {
-    // handle error
-    console.log('ERROR!!!!!!!!!!!!!!', error);
-  })
-
-
-})
-
-console.log('savedData: ', savedData)
-
-// const word = 'ace'
-
-// const URL = `https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${word}?strictMatch=false`
-
-// axios.get(URL, { headers: { app_id: "6a753563", app_key: "4ed66b63acb3c546838c88c7ab5d3c12" }})
-//   .then(function (response) {
-//     // handle success
-//     console.log(response.data);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-
-
-  
+const hundredWords = words.slice(0, 49)
+fetchWordInfos(hundredWords)
---
+++ utils.js
@@ -0,0 +1,111 @@
+const axios = require("axios");
+const fs = require("fs");
+const config = require('./config')
+
+const formatJsonData = json => {
+  const formattedData = json.reduce((reducedData, object) => {
+    const word = object["Word"];
+    const synonyms = [
+      object["Synonym 1"],
+      object["Synonym 2"],
+      object["Synonym 3"],
+      object["Synonym 4"]
+    ];
+    const antonyms = [
+      object["Antonym 1"],
+      object["Antonym 2"],
+      object["Antonym 3"],
+      object["Antonym 4"]
+    ];
+
+    const removedBlankSynonyms = synonyms.reduce((reducedSynonyms, synonym) => {
+      return synonym === ""
+        ? [...reducedSynonyms]
+        : [...reducedSynonyms, synonym];
+    }, []);
+
+    const removedBlankAntonyms = antonyms.reduce((reducedAntonyms, antonym) => {
+      return antonym === ""
+        ? [...reducedAntonyms]
+        : [...reducedAntonyms, antonym];
+    }, []);
+
+    return [
+      ...reducedData,
+      { word, synonyms: removedBlankSynonyms, antonyms: removedBlankAntonyms }
+    ];
+  }, []);
+
+  return formattedData;
+};
+
+
+
+
+const extractRootWords = data => {
+  const rootWords = data.map(wordObject => {
+    return wordObject.Word.trim()
+  })
+  console.log(rootWords)
+  return rootWords
+}
+
+
+
+
+async function fetchWordInfos(words) {
+  const promises = words.map(async word => {
+   
+    const URL = `https://od-api.oxforddictionaries.com/api/v2/thesaurus/en-gb/${word}?strictMatch=false`;
+
+      const response = await axios.get(URL, {
+        headers: {
+          app_id: "6a753563",
+          app_key: "4ed66b63acb3c546838c88c7ab5d3c12"
+        }
+      });
+
+    return response.data.results;
+  })
+  
+
+  const results = await Promise.all(promises);
+  console.log(results[0][0].lexicalEntries[0].entries[0].senses[0].synonyms);
+
+  fs.writeFile("./newData.json", JSON.stringify(results), err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    //file written successfully
+  });
+  return results;
+}
+
+
+async function fetchWordInfosRecursive(words) {
+
+  
+  recursiveApiCall(words, 0, [])
+
+  
+  const recursiveApiCall = (words, n, acc) => {
+    if( n > words.length - 1 ){
+      return acc
+    } else {
+      const word = words[n]
+      const URL = `https://od-api.oxforddictionaries.com/api/v2/thesaurus/en-gb/${word}?strictMatch=false`;
+  
+      axios.get(URL, config).then(response => {
+        acc.push(response.data.results)
+        recursiveApiCall(words, n + 1, acc)
+      })
+    }
+
+  }
+  
+}
+
+
+module.exports = { formatJsonData, fetchWordInfos, extractRootWords }
+
